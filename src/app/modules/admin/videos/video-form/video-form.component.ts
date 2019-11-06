d9774ca8c3c8e3bc79b3de60dcd8f7874e4393d0
Alisson Schmitz <alissonschmitz2@hotmail.com>
2019-11-05 22:48:17 -0300

    Init fields

--- src/app/modules/admin/videos/video-form/video-form.component.ts
+++ src/app/modules/admin/videos/video-form/video-form.component.ts
@@ -1,15 +1,92 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Video } from 'src/app/core/models/Video.model';
+import { Form } from 'src/app/shared/common';
+
+import { AlertService } from './../../../../core/services/alert.service';
+import { VideoService } from './../../../../core/services/video.service';
+import { Message } from './../../../../shared/common';
+import { FormHelper } from './../../../../shared/form-helper';
 
 @Component({
   selector: 'app-video-form',
   templateUrl: './video-form.component.html',
   styleUrls: ['./video-form.component.scss']
 })
-export class VideoFormComponent implements OnInit {
+export class VideoFormComponent extends Form implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup = new FormGroup({
+    title: new FormControl(),
+    description: new FormControl(),
+    archive: new FormControl(),
+    shared: new FormControl(),
+    teacher: new FormControl(),
+    active: new FormControl(true)
+  });
+
+  modelId: number;
+
+  constructor(alertService: AlertService, private videoService: VideoService, private router: Router, route: ActivatedRoute) {
+    super(alertService);
+
+    this.modelId = route.snapshot.params.id;
+  }
 
   ngOnInit() {
+    if (this.modelId) {
+      this.videoService.get(this.modelId)
+        .pipe(takeUntil(this.ngUnsubscribe))
+        .subscribe((res) => FormHelper.setFormGroupValues(this.formGroup, res));
+    }
+  }
+
+  onSave(close?: boolean) {
+    if (!this.validateForm(this.formGroup)) {
+      return;
+    }
+
+    const video = new Video();
+    video.deserialize(FormHelper.getValuesFromFormGroup(this.formGroup));
+
+    let action$: Observable<any>;
+
+    if (this.modelId) {
+      action$ = this.videoService.put(this.modelId, video);
+    } else {
+      action$ = this.videoService.post(video);
+    }
+
+    action$
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(
+        res => {
+
+          this.emitSuccessMessage(
+            this.modelId
+            ? Message.SUCCESSFUL_REGISTRY_EDITION
+            : Message.SUCCESSFUL_REGISTRY_INSERTION);
+
+          // When save & close
+          if (close) {
+            this.router.navigate([`/admin/videos`]);
+
+          // When save only
+          } else {
+            // When is a new registry, redirect to update
+            if (!this.modelId) {
+              this.router.navigate([`/admin/video/update/${res.id}`]);
+            }
+          }
+        },
+        error => this.emitErrorMessage(error)
+      );
+  }
+
+  onCancel() {
+    this.router.navigate([`/admin/videos`]);
   }
 
 }
