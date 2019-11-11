9496dd35143327eb2124f6de8f7321c05dc6cdff
Edvaldo da Rosa <fraterblack@bck.com.br>
2019-11-10 22:32:31 -0300

    Example to implement component

--- src/app/modules/admin/settings/update-my-account/update-my-account.component.ts
+++ src/app/modules/admin/settings/update-my-account/update-my-account.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { FormComponent } from 'src/app/shared/common';
 
@@ -9,6 +10,24 @@ import { AlertService } from './../../../../core/services/alert.service';
 import { AuthService } from './../../../../core/services/auth.service';
 import { UserService } from './../../../../core/services/user.service';
 import { Message } from './../../../../shared/common';
+import {
+  RelatedItem,
+  RelatedItemAction,
+  RelatedItemActionType
+} from './../../../../shared/components/related-items-selector/related-item-selector';
+
+const ELEMENT_DATA: RelatedItem[] = [
+  { id: 1, position: 1, title: 'Hydrogen' },
+  /*{ id: 2, position: 2, title: 'Helium' },
+  { id: 3, position: 3, title: 'Lithium' },
+  { id: 4, position: 4, title: 'Beryllium' },
+  { id: 5, position: 5, title: 'Boron' },
+  { id: 6, position: 6, title: 'Carbon' },
+  { id: 7, position: 7, title: 'Nitrogen' },
+  { id: 8, position: 8, title: 'Oxygen' },
+  { id: 9, position: 9, title: 'Fluorine' },
+  { id: 10, position: 10, title: 'Neon' },*/
+];
 
 @Component({
   selector: 'app-update-my-account',
@@ -26,6 +45,29 @@ export class UpdateMyAccountComponent extends FormComponent implements OnInit {
   });
 
   private userId: number;
+
+  // Stard here
+  dataSource = ELEMENT_DATA;
+  relatedData: Subject<RelatedItem[]> = new Subject<RelatedItem[]>();
+
+  onAction(action: RelatedItemAction) {
+    console.log(action);
+
+    // When notified by the component that is done to populate
+    if (action.type === RelatedItemActionType.LOADED) {
+      this.relatedData.next(this.dataSource);
+      this.relatedData.asObservable();
+
+      return;
+    }
+
+    this.dataSource.push({ id: 2, position: 2, title: 'Helium' });
+
+    this.relatedData.next(this.dataSource);
+    this.relatedData.asObservable();
+  }
+
+  // Until here
 
   constructor(alertService: AlertService, private userService: UserService, authService: AuthService) {
     super(alertService);
