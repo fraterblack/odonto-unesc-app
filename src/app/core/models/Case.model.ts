6f4fc7c71603e15f8ac90b0b03df7f17e54cbbd1
Alisson Schmitz <alissonschmitz2@hotmail.com>
2019-11-13 23:28:49 -0300

    Add model, service and initial implementation of grid/form

---
+++ src/app/core/models/Case.model.ts
@@ -0,0 +1,13 @@
+import { Model } from './model';
+import { User } from './User.model';
+
+export class Case extends Model {
+  id: number;
+  title: string;
+  description?: string;
+  shared?: boolean;
+  active?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+  teacher: User;
+}
